60221f8c3421724a177f24030b3ee3269c052cde
VedantS <vedant.shukla@knacksystems.com>
2025-11-05 17:29:07 +0530

    Updated PLP test cases and added TC_07_Electrolux_Laundry_Sort_By

--- src/test/java/com/automation/electrolux/tests/EPIC_04_PLP/TC_06_Electrolux_Laundry_Filters.java
+++ src/test/java/com/automation/electrolux/tests/EPIC_04_PLP/TC_06_Electrolux_Laundry_Filters.java
@@ -17,7 +17,7 @@ public class TC_06_Electrolux_Laundry_Filters extends BaseTest {
     ProductCategoryPageActions productCategoryPageActions = new ProductCategoryPageActions();
     PLPProductItemsPageActions PLPProductItemsPageActions = new PLPProductItemsPageActions();
 
-    public void navigateToVacuumsProducts(String webSite, String prodCategory, String subProdCategory) throws InterruptedException {
+    public void navigateToLaundryProducts(String webSite, String prodCategory, String subProdCategory) throws InterruptedException {
         homePage.navigateToHomePage();
         productCategoryPageActions.clickOnProductMenu(webSite, prodCategory);
         productCategoryPageActions.clickOnProductMenu(webSite, subProdCategory);
@@ -31,7 +31,7 @@ public class TC_06_Electrolux_Laundry_Filters extends BaseTest {
 
     @Test(groups = {"smoke", "regression"}, description = " Verify Hide and Show Filter for Laundry ")
     public void testHideShowFilters() throws InterruptedException {
-        navigateToVacuumsProducts("electrolux", "Laundry", "Washers");
+        navigateToLaundryProducts("electrolux", "Laundry", "Washers");
         PLPProductItemsPageActions.verifyHideAndShowAllFilters("Hide filters");
         PLPProductItemsPageActions.verifyHideAndShowAllFilters("Show filters");
         PLPProductItemsPageActions.verifyHideFilters("Hide filters");
@@ -43,7 +43,7 @@ public class TC_06_Electrolux_Laundry_Filters extends BaseTest {
 
     @Test(groups = {"regression"}, description = "Verify every filter expand and collapse")
     public void testExpandAndCollapseFilter() throws InterruptedException {
-        navigateToVacuumsProducts("electrolux", "Laundry", "Washers");
+        navigateToLaundryProducts("electrolux", "Laundry", "Washers");
         PLPProductItemsPageActions.verifyExpandAndCollapseFilter("electrolux", "Color", "Alpine");
         PLPProductItemsPageActions.verifyExpandAndCollapseFilter("electrolux", "Features", "High Efficiency");
         PLPProductItemsPageActions.verifyExpandAndCollapseFilter("electrolux", "Model", "700 Series");
@@ -55,7 +55,7 @@ public class TC_06_Electrolux_Laundry_Filters extends BaseTest {
 
     @Test(groups = {"regression"}, description = "Verify reset all filter")
     public void testResetAll() throws InterruptedException {
-        navigateToVacuumsProducts("electrolux", "Laundry", "Washers");
+        navigateToLaundryProducts("electrolux", "Laundry", "Washers");
         PLPProductItemsPageActions.featureFilter(electroxWeb, "Features", "High Efficiency");
         PLPProductItemsPageActions.verifySelectedFilters("High Efficiency");
         ExtentReportManager.getTest().pass("Successfully Verify reset all filter");
@@ -64,7 +64,7 @@ public class TC_06_Electrolux_Laundry_Filters extends BaseTest {
 
     @Test(groups = {"regression"}, description = "Verify Ice maker filter with its functionality")
     public void verifyPLPUpdatesWhenSingleFilterSelected() throws InterruptedException {
-        navigateToVacuumsProducts("electrolux", "Laundry", "Washers");
+        navigateToLaundryProducts("electrolux", "Laundry", "Washers");
         PLPProductItemsPageActions.featureFilter(electroxWeb, "Features", "High Efficiency");
         PLPProductItemsPageActions.openAllProductsAndValidate("Certifications and Approvals", "High Efficiency", "Yes");
         ExtentReportManager.getTest().pass("Verify Ice maker filter with its functionality");
@@ -74,7 +74,7 @@ public class TC_06_Electrolux_Laundry_Filters extends BaseTest {
 
     @Test(groups = {"regression"}, description = "Verify Model filter for 700 Series")
     public void verifyPLPUpdatesForModelFeature() throws InterruptedException {
-        navigateToVacuumsProducts("electrolux", "Laundry", "Washers");
+        navigateToLaundryProducts("electrolux", "Laundry", "Washers");
         PLPProductItemsPageActions.featureFilter(electroxWeb, "Model", "700 Series");
         PLPProductItemsPageActions.openAllProductsAndValidate("General Specifications", "Laundry Series", "700 Series");
         ExtentReportManager.getTest().pass("Verify Model filter for 700 Series");
@@ -83,7 +83,7 @@ public class TC_06_Electrolux_Laundry_Filters extends BaseTest {
     @Test(groups = {"smoke", "regression"}, description = "verify Availability of products in PLP")
     public void verify_Availaibility_In_PLP() throws InterruptedException
     {
-        navigateToVacuumsProducts("electrolux", "Laundry", "Washers");
+        navigateToLaundryProducts("electrolux", "Laundry", "Washers");
         PLPProductItemsPageActions.featureFilter("electrolux", "In stock", "Yes");
         PLPProductItemsPageActions.validateAvailabilityOfProductsInPLP();
         ExtentReportManager.getTest().pass("Successfully verify Availability of products in PLP");
