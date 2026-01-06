f64da26a0561f74544df534141849adc68f26604
VedantS <vedant.shukla@knacksystems.com>
2026-01-06 15:31:17 +0530

    code for Sort By FSUS

---
+++ src/test/java/com/automation/fsus/EPIC_04_PLP/EPIC_04_TC_02_PLP_Filters_Vacuums_Fsus.java
@@ -0,0 +1,4 @@
+package com.automation.fsus.EPIC_04_PLP;
+
+public class EPIC_04_TC_02_PLP_Filters_Vacuums_Fsus {
+}
---
+++ src/test/java/com/automation/fsus/EPIC_04_PLP/EPIC_04_TC_03_PLP_SortBy_Vacuums_Fsus.java
@@ -0,0 +1,4 @@
+package com.automation.fsus.EPIC_04_PLP;
+
+public class EPIC_04_TC_03_PLP_SortBy_Vacuums_Fsus {
+}
